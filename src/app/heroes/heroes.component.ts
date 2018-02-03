f99835d55d97599e8a836fc5a0af7296465f5aa1
tofu511 <mokmok511@gmail.com>
2018-02-03 18:19:27 +0900

    implement display list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero'; // Heroクラスをインポート
+import {HEROES } from '../mock-heroes';
 
 // @Componentは、コンポーネントのAngularメタデータを指定するデコレータ関数
 @Component({
@@ -7,15 +8,21 @@ import { Hero } from '../hero'; // Heroクラスをインポート
   templateUrl: './heroes.component.html', // コンポーネントのテンプレートファイルの場所
   styleUrls: ['./heroes.component.css'] // コンポーネントのプライベートCSSスタイルの場所 
 })
+
+
+
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+
+  selectedHero: Hero;
+  heroes = HEROES;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
+
