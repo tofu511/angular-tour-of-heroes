96fa53edc2392928f6f52f585103e902d3b3aca8
tofu511 <mokmok511@gmail.com>
2018-02-03 17:48:57 +0900

    implement two-way binding

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero'; // Heroクラスをインポート
 
+// @Componentは、コンポーネントのAngularメタデータを指定するデコレータ関数
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  selector: 'app-heroes', // コンポーネントのCSS要素セレクタ
+  templateUrl: './heroes.component.html', // コンポーネントのテンプレートファイルの場所
+  styleUrls: ['./heroes.component.css'] // コンポーネントのプライベートCSSスタイルの場所 
 })
 export class HeroesComponent implements OnInit {
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
 
   constructor() { }
 
